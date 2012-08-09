9a6a5ec9c04e33a1ba493932f8694085f8b589a0
imac <imac@linmatoiMac.local>
2012-08-09 16:16:07 +0800

    Initial Commit

---
+++ browserHD/RCViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCViewController.h
+//  browserHD
+//
+//  Created by imac on 12-8-9.
+//  Copyright (c) 2012年 2345. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCViewController : UIViewController
+
+@end
---
+++ browserHD/RCViewController.m
@@ -0,0 +1,34 @@
+//
+//  RCViewController.m
+//  browserHD
+//
+//  Created by imac on 12-8-9.
+//  Copyright (c) 2012年 2345. All rights reserved.
+//
+
+#import "RCViewController.h"
+
+@interface RCViewController ()
+
+@end
+
+@implementation RCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
